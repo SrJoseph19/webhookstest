b37f6260f95f42acb4243083c3d90fa0b684ac39
SrJoseph19 <xxdragonxtxx@gmail.com>
2025-09-03 08:30:17 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,50 @@
+const CACHE_NAME = 'discord-uploader-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/Styles/Styles.css',
+  '/Styles/ImgPrevStyle.css',
+  '/Scripts/Script.js',
+  '/Scripts/ImgPrevScript.js',
+  '/images/default-avatar.png',
+  '/images/default-avatar1.png',
+  '/images/default-avatar2.png'
+];
+
 self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
